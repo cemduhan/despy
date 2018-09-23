1c243e7d3036e744ce1a7e7d883f11a37997c243
D. Cem Karagöz <dhncem@yandex.com>
2018-09-23 13:43:50 +0300

    Changed lettering to comply with pep8 added a GasStation example

---
+++ GasStationExample.py
@@ -0,0 +1,34 @@
+import despy as des
+from matplotlib import pyplot
+from matplotlib.pyplot import hist,title,xlabel,ylabel,xlim,ylim
+
+def GasStation():   
+    print("EmpiricalGenerate_Test_Valid_File")
+    des.state.clear()
+    des.state.debugging=False
+    des.state.add_block(des.GenerateBlock('Uniform',5.5, 12.3, 1.5))  #0 Cars arrive between 5.5 and 12.3 seconds
+    des.state.add_block(des.EnterBlock("GasStation", 5))  #1 Cars Enter gas station which has 5 gas pumps so sixth car must wait
+    des.state.add_block(des.AdvanceBlock('Uniform',5,15))  #2 Filling the gas tank of a vehicle takes between 5 and 15 seconds
+    des.state.add_block(des.EnterBlock("Cashier", 2))  #3 After filling gas tank car owner must enter the line for payment
+    des.state.add_block(des.AdvanceBlock('Uniform',3,10))  #4 Payment takes between 3 and 10 seconds
+    des.state.add_block(des.LeaveBlock("Cashier"))  #5 After paying owner leaves the cashier line
+    des.state.add_block(des.LeaveBlock("GasStation"))  #6 After all owner leaves the station
+    des.state.add_block(des.TerminateBlock(1))  #7 Car destroyed
+
+    des.state.terminate_counter = 5
+
+
+#######################################################################################
+def experiment():
+
+    seed = 987654321
+    des.random.seed(seed)
+
+    GasStation()
+
+    des.simulate()
+
+
+if __name__=="__main__":
+
+    experiment()
--- tester.py
+++ tester.py
@@ -1,124 +1,124 @@
-import despy as DES
+import despy as des
 from matplotlib import pyplot
 from matplotlib.pyplot import hist,title,xlabel,ylabel,xlim,ylim
 
 def EmpiricalGenerate_Test_Valid_File():
     print("EmpiricalGenerate_Test_Valid_File")
-    DES.state.clear()
-    DES.state.debugging=False
-    DES.state.add_block(DES.GenerateBlock('Empirical', "distru.txt", False))#0
-    DES.state.add_block(DES.TerminateBlock(1))#2
+    des.state.clear()
+    des.state.debugging=False
+    des.state.add_block(des.GenerateBlock('Empirical', "distru.txt", False))#0
+    des.state.add_block(des.TerminateBlock(1))#2
 
-    DES.state.terminate_counter = 5
+    des.state.terminate_counter = 5
 
 def EmpiricalGenerate_Test_With_False_File():
     print("EmpiricalGenerate_Test_With_False_File")
-    DES.state.clear()
-    DES.state.debugging=False
-    DES.state.add_block(DES.GenerateBlock('Empirical', "distrust.txt", False))#0
-    DES.state.add_block(DES.TerminateBlock(1))#2
+    des.state.clear()
+    des.state.debugging=False
+    des.state.add_block(des.GenerateBlock('Empirical', "distrust.txt", False))#0
+    des.state.add_block(des.TerminateBlock(1))#2
 
-    DES.state.terminate_counter = 5
+    des.state.terminate_counter = 5
 #######################################################################################
 def UniformGenerate_Test():
     print("UniformGenerate_Test")
-    DES.state.clear()
-    DES.state.debugging=False
-    DES.state.add_block(DES.GenerateBlock('Uniform',5.5, 12.3, 1.5))#0
-    DES.state.add_block(DES.TerminateBlock(1))#2
+    des.state.clear()
+    des.state.debugging=False
+    des.state.add_block(des.GenerateBlock('Uniform',5.5, 12.3, 1.5))#0
+    des.state.add_block(des.TerminateBlock(1))#2
 
-    DES.state.terminate_counter = 5
+    des.state.terminate_counter = 5
 #######################################################################################
 def TerminateBlock_Test_With_Invalid_Limit():
     print("TerminateBlock_Test_With_Invalid_Limit")
-    DES.state.clear()
-    DES.state.debugging=False
-    DES.state.add_block(DES.GenerateBlock('NoDelay',5))#0
-    DES.state.add_block(DES.TerminateBlock(-2))#2
+    des.state.clear()
+    des.state.debugging=False
+    des.state.add_block(des.GenerateBlock('NoDelay',5))#0
+    des.state.add_block(des.TerminateBlock(-2))#2
 
-    DES.state.terminate_counter = 5
+    des.state.terminate_counter = 5
 
 def TerminateBlock_Test_With_Valid_Limit():
     print("TerminateBlock_Test_With_Valid_Limit")
-    DES.state.clear()
-    DES.state.debugging=False
-    DES.state.add_block(DES.GenerateBlock('NoDelay',5))#0
-    DES.state.add_block(DES.TerminateBlock(2))#2
+    des.state.clear()
+    des.state.debugging=False
+    des.state.add_block(des.GenerateBlock('NoDelay',5))#0
+    des.state.add_block(des.TerminateBlock(2))#2
 
-    DES.state.terminate_counter = 5
+    des.state.terminate_counter = 5
 #######################################################################################
 def NoDelayGenerate_Test_With_Valid_Limit():
     print("NoDelayGenerate_Test")
-    DES.state.clear()
-    DES.state.debugging=False
-    DES.state.add_block(DES.GenerateBlock('NoDelay',5))#0
-    DES.state.add_block(DES.TerminateBlock(1))#2
+    des.state.clear()
+    des.state.debugging=False
+    des.state.add_block(des.GenerateBlock('NoDelay',5))#0
+    des.state.add_block(des.TerminateBlock(1))#2
 
-    DES.state.terminate_counter = 5
+    des.state.terminate_counter = 5
 
 def NoDelayGenerate_Test_With_Invalid_Limit():
     print("NoDelayGenerate_Test")
-    DES.state.clear()
-    DES.state.debugging=False
-    DES.state.add_block(DES.GenerateBlock('NoDelay',-2))#0
-    DES.state.add_block(DES.TerminateBlock(1))#2
+    des.state.clear()
+    des.state.debugging=False
+    des.state.add_block(des.GenerateBlock('NoDelay',-2))#0
+    des.state.add_block(des.TerminateBlock(1))#2
 
-    DES.state.terminate_counter = 5
+    des.state.terminate_counter = 5
 #######################################################################################
 def EnterBlockTest_Valid_Que_Size():
     print("EnterBlockTest_Valid_Que_Size")
-    DES.state.clear()
-    DES.state.debugging=True
-    DES.state.add_block(DES.GenerateBlock('Uniform',2, 2, 1.2))#0
-    DES.state.add_block(DES.EnterBlock("Depot", 2))#1
-    DES.state.add_block(DES.AdvanceBlock('Uniform',5,5))#2
-    DES.state.add_block(DES.LeaveBlock("Depot"))#3
-    DES.state.add_block(DES.TerminateBlock(1))#4
+    des.state.clear()
+    des.state.debugging=True
+    des.state.add_block(des.GenerateBlock('Uniform',2, 2, 1.2))#0
+    des.state.add_block(des.EnterBlock("Depot", 2))#1
+    des.state.add_block(des.AdvanceBlock('Uniform',5,5))#2
+    des.state.add_block(des.LeaveBlock("Depot"))#3
+    des.state.add_block(des.TerminateBlock(1))#4
 
-    DES.state.terminate_counter = 3
+    des.state.terminate_counter = 3
 
 def EnterBlockTest_Invnalid_Que_Size():
     print("EnterBlockTest_Invnalid_Que_Size")
-    DES.state.clear()
-    DES.state.debugging=False
-    DES.state.add_block(DES.GenerateBlock('Uniform',2, 2, 1.2))#0
-    DES.state.add_block(DES.EnterBlock("Depot", -2))#1
-    DES.state.add_block(DES.AdvanceBlock('Uniform',10,10))#2
-    DES.state.add_block(DES.LeaveBlock("Depot"))#3
-    DES.state.add_block(DES.TerminateBlock(1))#4
+    des.state.clear()
+    des.state.debugging=False
+    des.state.add_block(des.GenerateBlock('Uniform',2, 2, 1.2))#0
+    des.state.add_block(des.EnterBlock("Depot", -2))#1
+    des.state.add_block(des.AdvanceBlock('Uniform',10,10))#2
+    des.state.add_block(des.LeaveBlock("Depot"))#3
+    des.state.add_block(des.TerminateBlock(1))#4
 
-    DES.state.terminate_counter = 10
+    des.state.terminate_counter = 10
 #######################################################################################
 def TransferBlock_Test_With_Everything_Passing():
     print("TransferBlock_Test_With_Everything_Passing")
-    DES.state.clear()
-    DES.state.debugging=False
-    DES.state.add_block(DES.GenerateBlock('Uniform',5, 5, 1.2))#0
-    DES.state.add_block(DES.TransferBlock(100,3))#1
-    DES.state.add_block(DES.TerminateBlock(2))#2
-    DES.state.add_block(DES.TerminateBlock(1))#3
+    des.state.clear()
+    des.state.debugging=False
+    des.state.add_block(des.GenerateBlock('Uniform',5, 5, 1.2))#0
+    des.state.add_block(des.TransferBlock(100,3))#1
+    des.state.add_block(des.TerminateBlock(2))#2
+    des.state.add_block(des.TerminateBlock(1))#3
 
-    DES.state.terminate_counter = 10
+    des.state.terminate_counter = 10
 
 def TransferBlock_Test_With_Nothing_Passing():
     print("TransferBlock_Test_With_Nothing_Passing")
-    DES.state.clear()
-    DES.state.debugging=False
-    DES.state.add_block(DES.GenerateBlock('Uniform',5, 5, 1.2))#0
-    DES.state.add_block(DES.TransferBlock(0,3))#1
-    DES.state.add_block(DES.TerminateBlock(2))#2
-    DES.state.add_block(DES.TerminateBlock(1))#3
+    des.state.clear()
+    des.state.debugging=False
+    des.state.add_block(des.GenerateBlock('Uniform',5, 5, 1.2))#0
+    des.state.add_block(des.TransferBlock(0,3))#1
+    des.state.add_block(des.TerminateBlock(2))#2
+    des.state.add_block(des.TerminateBlock(1))#3
 
-    DES.state.terminate_counter = 10
+    des.state.terminate_counter = 10
 #######################################################################################
 def experiment():
 
     seed = 987654321
-    DES.random.seed(seed)
+    des.random.seed(seed)
 
     EnterBlockTest_Valid_Que_Size()
 
-    DES.simulate()
+    des.simulate()
 
 
 if __name__=="__main__":
