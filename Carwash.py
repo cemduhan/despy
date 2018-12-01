c5b0a2522ea49583c0f72304f5a80c54905ab8f9
D. Cem Karagöz <dhncem@yandex.com>
2018-12-01 14:13:56 +0300

    Carwash Example

---
+++ Carwash.py
@@ -0,0 +1,28 @@
+import despy as des
+
+
+def carwash():
+    des.clear()
+
+    des.Storage("Carwash", 1)
+    # There is only one carwash slot
+    des.GenerateBlock("GenerateCars", "Uniform", 0, 14)
+    # Cars get in the carwash every 0 to 14 minutes
+    des.EnterBlock("CarwashEnter", "Carwash", 1)
+    # Car enters the carwash
+    des.AdvanceBlock("WashCar", "Uniform", 5, 5)
+    # A Car takes 5 minutes to wash
+    des.LeaveBlock("LeaveCarwash", "Carwash", 1)
+    # Car leaves the carwash
+    des.TerminateBlock("CarLeft", 1)
+    # Car leaves the simulation
+    des.set_terminate(6)
+    # Stop the simulation after 6  Cars Left
+
+    des.simulate()
+
+
+if __name__ == "__main__":
+    carwash()
+
+
