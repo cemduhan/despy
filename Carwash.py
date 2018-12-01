3db7238e4f834264ebf5aecca7ba1ccd5a458fbe
D. Cem Karagöz <dhncem@yandex.com>
2018-12-01 22:41:27 +0300

    Updated Model

--- Carwash.py
+++ Carwash.py
@@ -3,15 +3,16 @@ import despy as des
 
 def carwash():
     des.clear()
+    des.debug(True)
 
     des.Storage("Carwash", 2)
     # There is only two carwash slot
-    des.GenerateBlock("GenerateCars", "Uniform", 0, 14)
-    # Cars get in the carwash every 0 to 14 minutes
+    des.GenerateBlock("GenerateCars", "Uniform", 0, 5)
+    # Cars get in the carwash every 0 to 5 minutes
     des.EnterBlock("CarwashEnter", "Carwash", 1)
     # Car enters the carwash
-    des.AdvanceBlock("WashCar", "Uniform", 5, 5)
-    # A Car takes 5 minutes to wash
+    des.AdvanceBlock("WashCar", "Uniform", 15, 15)
+    # A Car takes 15 minutes to wash
     des.LeaveBlock("LeaveCarwash", "Carwash", 1)
     # Car leaves the carwash
     des.TerminateBlock("CarLeft", 1)
